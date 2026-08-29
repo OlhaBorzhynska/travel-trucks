a13e417038611944847c11b9d743190dd99fadba
OlyaBorzynska <mykytlo@gmail.com>
2026-08-29 14:46:51 +0300

    add metadata

--- app/catalog/[camperId]/page.tsx
+++ app/catalog/[camperId]/page.tsx
@@ -1,9 +1,23 @@
+import type { Metadata } from "next";
 import CamperPageContent from "@/components/CamperPageContent/CamperPageContent";
+import { getCamperById } from "@/lib/api/campers";
 
 interface CamperPageProps {
   params: Promise<{
     camperId: string;
   }>;
+}
+
+export async function generateMetadata({
+  params,
+}: CamperPageProps): Promise<Metadata> {
+  const { camperId } = await params;
+  const camper = await getCamperById(camperId);
+
+  return {
+    title: `${camper.name} | TravelTrucks`,
+    description: camper.description,
+  };
 }
 
 export default async function CamperPage({ params }: CamperPageProps) {
--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,6 +1,13 @@
+import type { Metadata } from "next";
 import css from "./catalog.module.css";
 import { Filters } from "@/components/Filters/Filters";
 import { CatalogContent } from "@/components/CatalogContent/CatalogContent";
+
+export const metadata: Metadata = {
+  title: "Camper Catalog | TravelTrucks",
+  description:
+    "Browse our camper catalog and find the perfect camper for your next adventure with TravelTrucks.",
+};
 
 export default function CatalogPage() {
   return (
