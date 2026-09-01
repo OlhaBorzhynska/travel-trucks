b58c9e1179187cc4e3f7f4803c1dec0a13b71c7b
OlyaBorzynska <mykytlo@gmail.com>
2026-09-01 09:13:41 +0300

    correct global fonts, hover btns, height filter section

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,8 +28,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-scroll-behavior="smooth">
-      <body className={`${inter.variable} ${manrope.variable}`}>
+    <html
+      lang="en"
+      data-scroll-behavior="smooth"
+      className={`${inter.variable} ${manrope.variable}`}
+    >
+      <body>
         <Providers>
           <Header />
           {children}
